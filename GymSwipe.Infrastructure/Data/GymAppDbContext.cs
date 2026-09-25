c98c03e032dc020a77e93abf72357938dfb08aea
Oscar Stenström <oscar.stenstrom2001@gmail.com>
2026-09-25 14:33:40 +0200

    Setting up dbset and configuring db context

---
+++ GymSwipe.Infrastructure/Data/GymAppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using GymSwipe.Domain.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymSwipe.Infrastructure.Data
+{
+    public class GymAppDbContext:DbContext
+    {
+        private readonly string _connstring = "Not handled yet";
+
+        public GymAppDbContext(DbContextOptions<GymAppDbContext> options) : base(options) { }
+
+        public DbSet<Exercise> Exercises { get; set; }
+        public DbSet<ExerciseRecords> ExerciseRecords { get; set; }
+        public DbSet<ExerciseTargetArea> ExercisesTargetAreas { get; set; }
+        public DbSet<GymPlaylist> GymPlaylists { get; set; }
+        public DbSet<GymUser> Users { get; set; }
+        public DbSet<PlaylistExcercise> UserPlaylistExercises { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
